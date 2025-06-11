6c54634fc1843d47bd4b3a4aca8d78066ae39698
Aitor Garcés-Manzanera <168669243+aitorgarces@users.noreply.github.com>
2025-06-11 16:46:05 +0200

    Add files via upload

---
+++ posts.js
@@ -0,0 +1,10 @@
+const posts = [
+    {
+        title: "Essential Statistical Concepts for L2 Education Research",
+        date: "17 de Diciembre de 2024",
+        image: "https://cdn.glitch.global/dbb76279-9080-4683-96a4-a2929ed450fd/blog1?v=1734460039996",
+        content: "Learn the fundamental statistical concepts—population, sample, and variables—necessary for conducting successful quantitative research in second language education. Explore practical examples and key insights to strengthen your research design.",
+        link: "publicacion1.html"
+    },
+
+];
